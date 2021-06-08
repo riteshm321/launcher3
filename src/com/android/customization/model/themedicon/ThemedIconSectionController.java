c21a3ffb6726945e8d56845eb326a1c835529fc9
Chihhang Chuang <chihhangchuang@google.com>
2021-06-08 12:53:24 +0800

    [1/n] Move new picker logic to AOSP
    - Dark/light Theme.
    - Themed Icon.
    - App Grid.
    
    Bug: 190354625
    Test: Build and run the app.
    Change-Id: I54c7c3897568019326b827091270091bc114611c

---
+++ src/com/android/customization/model/themedicon/ThemedIconSectionController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.customization.model.themedicon;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import androidx.annotation.Nullable;
+
+import com.android.customization.model.HubSectionController;
+import com.android.customization.picker.themedicon.ThemedIconSectionView;
+import com.android.wallpaper.R;
+
+/** The {@link HubSectionController} for themed icon section. */
+public class ThemedIconSectionController implements HubSectionController<ThemedIconSectionView> {
+
+    private final ThemedIconSwitchProvider mThemedIconOptionsProvider;
+    private final ThemedIconViewModel mThemedIconViewModel;
+
+    public ThemedIconSectionController(ThemedIconSwitchProvider themedIconOptionsProvider,
+            ThemedIconViewModel themedIconViewModel) {
+        mThemedIconOptionsProvider = themedIconOptionsProvider;
+        mThemedIconViewModel = themedIconViewModel;
+    }
+
+    @Override
+    public boolean isAvailable(@Nullable Context context) {
+        return context != null && mThemedIconOptionsProvider.isThemedIconAvailable();
+    }
+
+    @Override
+    public ThemedIconSectionView createView(Context context) {
+        ThemedIconSectionView themedIconColorSectionView =
+                (ThemedIconSectionView) LayoutInflater.from(context).inflate(
+                        R.layout.themed_icon_section_view, /* root= */ null);
+        themedIconColorSectionView.setViewListener(this::onViewActivated);
+        themedIconColorSectionView.getSwitch()
+                .setChecked(mThemedIconOptionsProvider.fetchThemedIconEnabled());
+        return themedIconColorSectionView;
+    }
+
+    private void onViewActivated(Context context, boolean viewActivated) {
+        if (context == null) {
+            return;
+        }
+        mThemedIconOptionsProvider.setThemedIconEnabled(viewActivated);
+        mThemedIconViewModel.getThemedIconEnabled().setValue(viewActivated);
+    }
+}

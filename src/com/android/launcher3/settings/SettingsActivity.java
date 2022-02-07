547913b489e75854680f897b4e379861e9fcd790
Pranav Vashi <neobuddy89@gmail.com>
2022-02-07 19:12:36 +0530

    Launcher3: Add toggle to theme dock searchbar icons
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id0a4c7177e38285849a7bc12186427ea446b10a6
    Signed-off-by: StarkDroid <ggamerprince.exp@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -144,7 +144,9 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                 Utilities.restart(this);
         } else if (Utilities.KEY_DOCK_SEARCH.equals(key)) {
                 Utilities.restart(this);
-        }
+        } else if (Utilities.KEY_DOCK_THEME.equals(key)) {
+                Utilities.restart(this);
+        } 
     }
 
     private boolean startPreference(String fragment, Bundle args, String key) {

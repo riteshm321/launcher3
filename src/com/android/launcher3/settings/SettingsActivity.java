bf8c5329e7b51b5ab8127c1ac39f95be0e5c8002
DennySPb <dennyspb@gmail.com>
2022-04-20 10:42:53 +0300

    Settings/IconPack: Dynamically set the icon of the selected pack
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ie04f34cd8865ecf4e0b2caa6b57e250a70817d44

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -31,6 +31,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -223,7 +224,8 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
         protected static final String DPS_PACKAGE = "com.google.android.as";
 
         private Preference mShowGoogleAppPref;
-         private Preference mShowGoogleBarPref;
+        private Preference mShowGoogleBarPref;
+        private ReloadingListPreference mIconPackPref;
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -340,12 +342,14 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                     return true;
 
                 case KEY_ICON_PACK:
-                    ReloadingListPreference icons = (ReloadingListPreference) findPreference(KEY_ICON_PACK);
-                    icons.setValue(IconDatabase.getGlobal(getActivity()));
-                    icons.setOnReloadListener(IconPackPrefSetter::new);
-                    icons.setOnPreferenceChangeListener((pref, val) -> {
+                    mIconPackPref = (ReloadingListPreference) preference;
+                    mIconPackPref.setValue(IconDatabase.getGlobal(getActivity()));
+                    mIconPackPref.setOnReloadListener(IconPackPrefSetter::new);
+                    mIconPackPref.setIcon(getPackageIcon(IconDatabase.getGlobal(getActivity())));
+                    mIconPackPref.setOnPreferenceChangeListener((pref, val) -> {
                         IconDatabase.clearAll(getActivity());
                         IconDatabase.setGlobal(getActivity(), (String) val);
+                        mIconPackPref.setIcon(getPackageIcon((String) val));
                         AppReloader.get(getActivity()).reload();
                         return true;
                     });
@@ -387,6 +391,17 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                 return false;
             }
         }
+
+        private Drawable getPackageIcon(String pkgName) {
+            Drawable icon = getContext().getResources().
+                              getDrawable(R.drawable.ic_launcher_home);
+            try {
+                 icon = getContext().getPackageManager().
+                              getApplicationIcon(pkgName);
+            } catch (PackageManager.NameNotFoundException e) {  }
+            return icon;
+        }
+
 
         @Override
         public void onResume() {

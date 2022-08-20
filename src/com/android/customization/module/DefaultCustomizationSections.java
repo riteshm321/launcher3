0ae8e2e4485d2c57d7f43ce4d37c58979eb458be
Tim Zimmermann <tim@linux4.de>
2022-08-20 19:13:19 +0200

    ThemePicker: Actually enable color picker
    Change-Id: Ifb3e3ff15bb3875dab1cae77df0af4caf960a908

--- src/com/android/customization/module/DefaultCustomizationSections.java
+++ src/com/android/customization/module/DefaultCustomizationSections.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.LifecycleOwner;
 
+import com.android.customization.model.color.ColorSectionController;
 import com.android.customization.model.grid.GridOptionsManager;
 import com.android.customization.model.grid.GridSectionController;
 import com.android.customization.model.mode.DarkModeSectionController;
@@ -40,6 +41,10 @@ public final class DefaultCustomizationSections implements CustomizationSections
                 activity, lifecycleOwner, permissionRequester, wallpaperColorsViewModel,
                 workspaceViewModel, sectionNavigationController, wallpaperPreviewNavigator,
                 savedInstanceState));
+
+        // Color section
+        sectionControllers.add(new ColorSectionController(
+                activity, wallpaperColorsViewModel, lifecycleOwner, savedInstanceState));
 
         // Dark/Light theme section.
         sectionControllers.add(new DarkModeSectionController(activity,
